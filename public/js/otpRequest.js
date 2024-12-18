71a11e4135fe1d82a1766311a7422535437e8e96
Abhay <abhay@only1loan.com>
2024-12-18 17:32:00 +0530

    otp input route corrected

--- public/js/otpRequest.js
+++ public/js/otpRequest.js
@@ -28,7 +28,7 @@ async function requestOtp() {
 
             localStorage.setItem("aadhaarInfo", JSON.stringify(aadhaarInfo));
             console.log(localStorage.getItem("aadhaarInfo"));
-            window.location.href = `/aadhaarVerificationTemplate/`;
+            window.location.href = `/aadhaarVerificationTemplate/verify-aadhaar-input`;
         } else {
             alert(result.message || "Failed to request OTP. Please try again.");
         }
